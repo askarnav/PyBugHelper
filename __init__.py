9795f753c44147ea53cae04d97d0f5bf3830ef7a
Arnav <iam.arnav.somani@gmail.com>
2026-08-17 15:06:55 +0530

    Project code added

--- __init__.py
+++ __init__.py
@@ -1,2 +1,14 @@
+# __init__.py
+
+"""PyBugHelper: A package to help you debug Python code."""
+
+__version__ = "2026.0.0"
+__author__ = "askarnav"
+
+# Print greeting when the package is imported
 print("PyBugHelper is here to help you!")
 print("PyBugHelper 2026 Super PYTHON - askarnav")
+
+# Expose core functionality here
+# from .debugger import BugHelper
+# __all__ = ["BugHelper"]
